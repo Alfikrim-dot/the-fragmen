ed6ea5838ea41bd6fee2bb405ea9cd255c352a52
Alfikri Ux <alfikriaramdhan@gmail.com>
2026-04-29 10:48:06 +0700

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,4 +1,14 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Navbar() {
+  const [allowed, setAllowed] = useState(false);
+
+  useEffect(() => {
+    const status = localStorage.getItem("enteredLibrary");
+    if (status === "true") setAllowed(true);
+  }, []);
+
   return (
     <nav className="w-full border-b border-neutral-800 backdrop-blur-md bg-black/40 sticky top-0 z-50">
       <div className="container flex justify-between items-center py-5 px-6">
@@ -8,11 +18,24 @@ export default function Navbar() {
         </h1>
 
         <div className="flex gap-8 text-sm text-neutral-500">
-          <a href="/" className="hover:text-white">Home</a>
-          <a href="/library" className="hover:text-white">Library</a>
-          <a href="/about" className="hover:text-white">About</a>
-        </div>
+          
+          {/* LIBRARY */}
+          {allowed ? (
+            <a href="/library" className="hover:text-white">
+              Library
+            </a>
+          ) : (
+            <span className="cursor-not-allowed opacity-40">
+              Library
+            </span>
+          )}
 
+          {/* ABOUT (tetap aktif) */}
+          <a href="/about" className="hover:text-white">
+            About
+          </a>
+
+        </div>
       </div>
     </nav>
   );
